d22af46c79fa6f1daa71b3d0856c0081087e56a6
Gideon Boakye <122945393+yungProg@users.noreply.github.com>
2024-11-12 23:07:51 +0000

    Accept negative shift

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,31 +1,37 @@
 def caesar_cipher(text, shift)
-    #check if `shift` is a number
     
+    #check if `shift` is a number
     if !shift.is_a?(Numeric)
         return "Invalid input"
     elsif shift.is_a?(Numeric)
         shift = shift.to_i
     end
-    wrap large numbers
+    #wrap large numbers
     if shift >= 26
         shift = shift % 26
     end
     #convert characters to code
     character_codes_array = text.bytes
     #encode
+    debugger
     shifted_codes = character_codes_array.map do |character_code|
         if (65..90).include?(character_code)
             character_code += shift
-            if character_code - 90 >= 1
+            if character_code > 90 
                 character_code -= 26
+            elsif character_code < 65
+                character_code += 26
             end
         elsif (97..122).include?(character_code)
             character_code += shift
-            if character_code - 122 >= 1
+            if character_code > 122
               character_code -= 26
+            elsif character_code < 97
+                character_code += 26
             end
         end
         character_code
     end
     shifted_codes.pack("C*")
 end
+caesar_cipher(44, 2)
